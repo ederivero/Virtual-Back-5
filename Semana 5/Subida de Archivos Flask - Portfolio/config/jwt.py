924944bf0cc911062add728f54c4ef13f841d942
Eduardo de Rivero <ederiveroman@gmail.com>
2021-03-04 20:30:35 -0500

    avance

--- Semana 5/Subida de Archivos Flask - Portfolio/config/jwt.py
+++ Semana 5/Subida de Archivos Flask - Portfolio/config/jwt.py
@@ -3,7 +3,17 @@ def manejo_error_jwt(error):
     print(error.status_code) # sirve para devolver el status code del error actual
     print(error.error) # me retorna el error que se ha sucitado 
     print(error.description) # la descripcion del error
-    print(error.headers) # la cabecera de ese error
-    return {
-        'success': False
-    }, 400
+    print(error.headers) # la cabecera de ese error (solamente aparece cuando no hay token)
+    respuesta = {
+        'success': False,
+        'content': None,
+        'message': None
+    }
+    if error.error == 'Invalid token':
+        respuesta['message'] = 'Token invalida'
+    elif error.error == 'Authorization Required':
+        respuesta['message'] = 'Se necesita una token para esta peticion'
+    else:
+        respuesta['message'] = 'Sucedio otro error, comuniquese con el backend'
+    
+    return respuesta, 401
--- Semana 5/Subida de Archivos Flask - Portfolio/controllers/redSocial.py
+++ Semana 5/Subida de Archivos Flask - Portfolio/controllers/redSocial.py
@@ -1,4 +1,5 @@
 # CREAR, EDITAR Y LEER (todas)
+from flask_jwt import jwt_required, current_identity
 from flask_restful import Resource, reqparse, request
 from models.redSocial import RedSocialModel
 
@@ -18,15 +19,26 @@ class RedSocialController(Resource):
         help='Falta el rs_imagen',
         location='json'
     )
+    @jwt_required()
     def post(self):
-        data = self.serializer.parse_args()
-        nuevaRedSocial = RedSocialModel(data['rs_nombre'], data['rs_imagen'])
-        nuevaRedSocial.save()
-        return {
-            'success': True,
-            'content': nuevaRedSocial.json(),
-            'message': 'Se creo la nueva red social'
-        }
+        print(current_identity)
+        # solamente un usuario que es super usuario puede hacer un registro, sino indicar que no tiene los privilegios suficientes
+        identidad_usuario = current_identity
+        if(identidad_usuario['usuario_superuser']):
+            data = self.serializer.parse_args()
+            nuevaRedSocial = RedSocialModel(data['rs_nombre'], data['rs_imagen'])
+            nuevaRedSocial.save()
+            return {
+                'success': True,
+                'content': nuevaRedSocial.json(),
+                'message': 'Se creo la nueva red social'
+            }
+        else:
+            return {
+                'success': False,
+                'content': None,
+                'message': 'Usuario no dispone de suficientes privilegios'
+            }, 500
     def put(self):
         pass
     def get(self):
