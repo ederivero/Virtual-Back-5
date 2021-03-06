c7ed71681a08941bf2cd8ff8c69fe3df75b9a91c
Eduardo de Rivero <ederiveroman@gmail.com>
2021-03-06 11:45:24 -0500

    avance

--- Semana 6/API-EXPRESS/dist/controllers/Temario.js
+++ Semana 6/API-EXPRESS/dist/controllers/Temario.js
@@ -1,17 +1,45 @@
 // let funcionar en el scope actual y no es reconocido fuera de el
 // var funciona de manera global en el documento
 // const funciona como una constante en la cual no se puede cambiar su valor despues de definirlo
-let temario = [];
+let temarios = [];
 const crearTemario = (req, res)=>{
     // el request sirve para recibir todo lo que el usuario nos este mandando
     console.log(req.body);
+    temarios.push(req.body);
     return res.json({
         success: true
     })
 }
-const devolverTemarios = (req, res)=>{}
+const devolverTemarios = (req, res)=>{
+    // devolver todos los temarios
+    return res.json({
+        success: true,
+        content: temarios,
+        message: null
+    })
+}
+
+const actualizarTemario = (req, res)=>{
+    // console.log(req.params); // devuelve todos los valores dados por el parametro
+    let {id} = req.params;
+    if (temarios[id]){
+        temarios[id] = req.body;
+        return res.json({
+            success: true,
+            content: temarios[id],
+            message: 'Temario actualizado exitosamente'
+        })
+    }else{
+        return res.status(404).json({
+            success: false,
+            content: null,
+            message: 'Temario no existe'
+        })
+    }
+}
 
 module.exports={
     crearTemario,
-    devolverTemarios
+    devolverTemarios,
+    actualizarTemario
 }
--- Semana 6/API-EXPRESS/dist/routes/Temario.js
+++ Semana 6/API-EXPRESS/dist/routes/Temario.js
@@ -1,8 +1,12 @@
 // esto se llama destructuracion y sirve para solamente usar una parte de una libreria, documento, etc.
 const {Router} = require('express')
-const {crearTemario} = require('../controllers/Temario')
+// PRIMER METODO
+// const {crearTemario, devolverTemarios, actualizarTemario} = require('../controllers/Temario')
+// SEGUNDO METODO
+const temarioController = require('../controllers/Temario')
 
 const temario_router = Router();
-temario_router.post('/temario', crearTemario)
-
+temario_router.post('/temario', temarioController.crearTemario)
+temario_router.get('/temario', temarioController.devolverTemarios)
+temario_router.put('/temario/:id', temarioController.actualizarTemario)
 module.exports = temario_router
