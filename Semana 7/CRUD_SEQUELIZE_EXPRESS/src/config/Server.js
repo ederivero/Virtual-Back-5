8b6d1a8881662867fd95905d4cc6757af5b4fb3b
Eduardo de Rivero <ederiveroman@gmail.com>
2021-03-08 19:15:47 -0500

    avance

--- Semana 7/CRUD_SEQUELIZE_EXPRESS/src/config/Server.js
+++ Semana 7/CRUD_SEQUELIZE_EXPRESS/src/config/Server.js
@@ -6,10 +6,23 @@ module.exports = class Server{
         this.app = express()
         this.puerto = process.env.PORT || 5000;
         this.bodyParser();
+        this.CORS()
         this.rutas()
     }
     bodyParser(){
         this.app.use(json())
+    }
+    CORS(){
+        // los cors son el control de acceso a nuestra api, aca definimos que dominios pueden acceder, que metodos se pueden acceder y que headers se puede enviar
+        this.app.use((req, res, next)=>{
+            // Access-Control-Allow-Origin => indica que dominio o dominios pueden acceder a mi API
+            res.header('Access-Control-Allow-Origin', '*')
+            // Access-Control-Allow-Headers => sirve para indicar que tipos de cabeceras me puede mandar el front
+            res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+            // Access-Control-Allow-Methods => sirve para indicar que metodos pueden acceder a mi API
+            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+            next()
+        })
     }
     rutas(){
         this.app.get('/',(req, res)=>{
