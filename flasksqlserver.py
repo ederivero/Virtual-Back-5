672c3ac45e8acb205d73549cfc378657bb601f45
Eduardo de Rivero <ederiveroman@gmail.com>
2021-03-17 22:18:20 -0500

    conexion flask sql server

---
+++ flasksqlserver.py
@@ -0,0 +1,35 @@
+# pip install flask
+# pip install pyodbc
+# pip install flask-sqlalchemy
+from flask import Flask
+import urllib
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+# usuario root
+# https://docs.sqlalchemy.org/en/14/dialects/mssql.html#sql-server-data-types
+params = urllib.parse.quote_plus(
+    'DRIVER={SQL Server};SERVER=EDUARDO-PC\SQLEXPRESS;DATABASE=pruebas;UID=usuario;PWD=root;Trusted_Connection=yes;')
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Prueba(db.Model):
+    __tablename__ = 't_prueba'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+
+db.create_all()
+
+app.run(debug=True)
