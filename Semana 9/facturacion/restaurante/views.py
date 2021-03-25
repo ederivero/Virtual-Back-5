8a64d00cd989b29e6efbb9f850aec68ab1a0cced
Eduardo de Rivero <ederiveroman@gmail.com>
2021-03-24 20:37:15 -0500

    avance plato controller

--- Semana 9/facturacion/restaurante/views.py
+++ Semana 9/facturacion/restaurante/views.py
@@ -8,11 +8,30 @@ class PlatosController(generics.ListCreateAPIView):
     serializer_class = PlatoSerializer
 
     def get(self, request):
+        respuesta = self.serializer_class(
+            instance=self.get_queryset(), many=True)
+
         return Response({
-            'success': True
+            'success': True,
+            'content': respuesta.data,
+            'message': None
         })
 
     def post(self, request):
-        return Response({
-            'success': True
-        })
+        # para ver que archivos me estan mandando puedo ingresar a su atributo FILES dentro de request
+        # https://www.django-rest-framework.org/api-guide/requests/#data
+        print(request.FILES['platoFoto'].size)
+        respuesta = self.serializer_class(data=request.data)
+        if respuesta.is_valid():
+            respuesta.save()
+            return Response({
+                'success': True,
+                'message': 'Se registro el plato exitosamente',
+                'content': respuesta.data
+            })
+        else:
+            return Response({
+                'success': False,
+                'message': 'Error al registrar el plato',
+                'content': respuesta.errors
+            }, status.HTTP_400_BAD_REQUEST)
